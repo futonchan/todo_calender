e3461bfa300e87983490e0a5cc5d4f3177c44fce
yano <assdroi555@gmail.com>
2021-02-19 17:40:16 +0900

    add checkmark

--- todo_calendar/TodoViewController.swift
+++ todo_calendar/TodoViewController.swift
@@ -17,20 +17,55 @@ class TodoViewController: UIViewController, UITableViewDataSource, UITableViewDe
     // 要求があったとき、todoListの行ごとの内容を返す
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
-        let todoTitle = todoList[indexPath.row]
-        cell.textLabel?.text = todoTitle
+        let myTodo = todoList[indexPath.row]
+        cell.textLabel?.text = myTodo.todoTitle
+        if myTodo.todoDone {
+            cell.accessoryType = UITableViewCell.AccessoryType.checkmark
+        }
+        else{
+            cell.accessoryType = UITableViewCell.AccessoryType.none
+        }
         return cell
     }
+    
+    // タスクをタップ時にチェックマークつける
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        let myTodo = todoList[indexPath.row]
+        if myTodo.todoDone{
+            myTodo.todoDone = false
+        }
+        else {
+            myTodo.todoDone = true
+        }
+        
+        tableView.reloadRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        do {
+            let data: Data = try NSKeyedArchiver.archivedData(withRootObject: todoList, requiringSecureCoding: true)
+            let userDefaults = UserDefaults.standard
+            userDefaults.set(data, forKey: "todoList")
+            userDefaults.synchronize()
+        }
+        catch{
+            // エラー処理なし
+        }
+    }
 
-    var todoList = [String]()
+    var todoList = [MyTodo]()
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let userDefaults = UserDefaults.standard
-        if let storedTodoList = userDefaults.array(forKey: "todoList") as? [String] {
-            todoList.append(contentsOf: storedTodoList)
+        if let storedTodoList = userDefaults.object(forKey: "todoList") as? Data{
+            do {
+                if let unarchiveTodoList = try NSKeyedUnarchiver.unarchivedObject(ofClasses: [NSArray.self, MyTodo.self], from: storedTodoList) as? [MyTodo] {
+                    todoList.append(contentsOf: unarchiveTodoList)
+                }
+            }
+            catch{
+                // エラー処理なし
+            }
         }
     }
     
@@ -42,12 +77,22 @@ class TodoViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
         let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default){ (action: UIAlertAction) in
             if let textField = alertController.textFields?.first {
-                self.todoList.insert(textField.text!, at: 0)
+                let myTodo = MyTodo()
+                myTodo.todoTitle = textField.text!
+                self.todoList.insert(myTodo, at: 0)
+                
                 self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableView.RowAnimation.right)
                 
                 let userDefaults = UserDefaults.standard
-                userDefaults.set(self.todoList, forKey: "todoList")
-                userDefaults.synchronize()
+                
+                do {
+                    let data = try NSKeyedArchiver.archivedData(withRootObject: self.todoList, requiringSecureCoding: true)
+                    userDefaults.set(data, forKey: "todoList")
+                    userDefaults.synchronize()
+                }
+                catch {
+                    // エラー処理なし
+                }
             }
         }
         alertController.addAction(okAction)
@@ -67,4 +112,26 @@ class TodoViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     */
 
+}
+
+class MyTodo: NSObject, NSSecureCoding {
+    static var supportsSecureCoding: Bool{
+        return true
+    }
+    
+    var todoTitle: String?
+    var todoDone: Bool = false
+    override init(){
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        todoTitle = aDecoder.decodeObject(forKey: "todoTitle") as? String
+        todoDone = aDecoder.decodeBool(forKey: "todoDone")
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(todoTitle, forKey: "todoTitle")
+        aCoder.encode(todoDone, forKey: "todoDone")
+    }
+    
 }
