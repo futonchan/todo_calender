1f3897e21e65c1a270799e348ef84194980963c0
yano <assdroi555@gmail.com>
2021-03-19 17:42:01 +0900

    add pointing dot on calendar same task datetime

--- todo_calendar/CalendarViewController.swift
+++ todo_calendar/CalendarViewController.swift
@@ -27,8 +27,10 @@ class CalendarViewController: UIViewController, UITableViewDataSource, UITableVi
         return cell
     }
     
+    // class内に書いてるのでself.でとってこれる変数たち
     var todoList = [MyTodo]() // UserDefaultsからとってくる全体のTodolist
     var today_todolist = [MyTodo]() // Calendar画面で描画されるtodoList
+    var datesWithEvent = [String]()
 
     @IBOutlet weak var calendar: FSCalendar!
     @IBOutlet weak var calendar_tableView: UITableView!
@@ -44,8 +46,9 @@ class CalendarViewController: UIViewController, UITableViewDataSource, UITableVi
     
     // CalendarViewControllerが描画される前に毎回呼び出される（画面遷移したときなど）
     override func viewWillAppear(_ animated: Bool) {
-        todoList = [] // UserDefaultsからとってきたtodoList全体を格納する
-
+        self.todoList = [] // UserDefaultsからとってきたtodoList全体を格納する
+        self.datesWithEvent = [] // selfつけるつけないはプログラムに影響なし、self付けた方がわかりやすい
+        
         // UserDefaultsから値取得する処理
         let userDefaults = UserDefaults.standard
         if let storedTodoList = userDefaults.object(forKey: "todoList") as? Data{
@@ -78,6 +81,7 @@ class CalendarViewController: UIViewController, UITableViewDataSource, UITableVi
             if let _ =  todo.todoDate{
                 print(todo.todoTitle!)
                 print(todo.todoDate!.split(separator: " ")[0])
+                datesWithEvent.append(String(todo.todoDate!.split(separator: " ")[0]))
                 // todoListの日付がカレンダーの初期選択日（今日）と一致したら
                 if todayString.split(separator: " ")[0] == todo.todoDate!.split(separator: " ")[0]{
                     // today_todolistにtodo追加
@@ -90,8 +94,30 @@ class CalendarViewController: UIViewController, UITableViewDataSource, UITableVi
         print(today_todolist.count)
         self.calendar_tableView.reloadData()
         
+        
     }
     
+    // ここの関数、明示的に呼び出したい
+    // delegateメソッドと言われるもので、呼び出しは向こうが勝手に決めてるっぽい
+    // タスクがある日を、カレンダー上へ設定した色で点をうつ
+    // カレンダー動かすなど、動きをしないと新しいタスクの点が描画されない
+    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short //元データは時刻もある？
+        dateFormatter.locale = Locale(identifier: "ja_JP") // Y/m/dになる
+        let dateString = dateFormatter.string(from: date)
+//        print("event point")
+//        print(dateString)
+//        print(datesWithEvent)
+        // dateswitheventがイベント日がある配列
+        if self.datesWithEvent.contains(String(dateString.split(separator: " ")[0])) {
+           return 1 // Here, We have to assign JSON count key value based on duplication it will increase dot count UI.
+        }
+        return 0
+    }
+    
+    // 現在選択している年月日をy/m/dで取得する
     func getDay(_ date:Date) -> String{
         let tmpCalendar = Calendar(identifier: .gregorian)
         let year = tmpCalendar.component(.year, from: date)
@@ -99,8 +125,10 @@ class CalendarViewController: UIViewController, UITableViewDataSource, UITableVi
         let day = tmpCalendar.component(.day, from: date)
         return String(year) + "/" + String(month) + "/" + String(day)
     }
+    
+    // カレンダーの日時が選択されたときに呼び出される
+    // 全体のtodoListから選択した日時のタスクのものをtableView上に表示する
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition){
-//        let selectDay = getDay(date)
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
